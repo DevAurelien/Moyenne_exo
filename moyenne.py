a598b6ecd28588baf1b2eebe50a8df8981e7bca6
User_Aurelien <lhuillier.aurelien47@gmail.com>
2024-06-01 19:20:40 +0200

    Update moyenne.py

--- moyenne.py
+++ moyenne.py
@@ -1,13 +1,4 @@
-# Tao = [18, 12, 3, 5, 19]
-# Josette = [20, 2, 12, 18, 14]
-# Patrick = [2, 4, 6, 18, 17]
-# Pema = [3, 19, 15, 3, 12]
-# Jean = [0, 9, 8, 8, 4]
-# Bixente = [14, 20, 10, 12, 4]
-# Paco = [16, 1, 1, 1, 20]
-# Chuluun = [15, 6, 17, 20, 15]
-# Marie = [16, 4, 16, 20, 12]
-# Mohamed = [16, 19, 17, 6, 20]
+
 chemin = "etudiant.txt"
 dictionnaire_objet = {}
 
@@ -45,11 +36,11 @@ def classement_eleves(dico_moyenne):
     i, compteur = 20, 1
     while not i == 0:
         for key, values in dico_moyenne.items():
-            if i==values:
+            if i == values:
                 print(
                 f"{compteur} : {key} avec une moyenne de {dico_moyenne[key]}/20")
                 compteur +=1
-        i-=1
+        i -= 1
 
 
 classement_eleves(dico_moyenne)
